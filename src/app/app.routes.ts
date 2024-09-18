3aebf75e0673801f48d5f3cd9ba1bd4653834e68
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2024-09-18 21:17:57 +0530

    Issue #5: Implement routing in application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'first-page', component: FirstComponent },
+  { path: 'second-page', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
